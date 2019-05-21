45477129661889358aafde7bb7450875a0542bf3
Felecia MAKGOKA <fmakgoka@c4r9s5.wethinkcode.co.za>
2019-05-21 15:35:49 +0200

    adding all

---
+++ libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmakgoka <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 14:05:02 by fmakgoka          #+#    #+#             */
+/*   Updated: 2019/05/21 15:32:19 by fmakgoka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_isalnum(int c);
+
+#endif
