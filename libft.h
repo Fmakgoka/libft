e6f299ae7e7abb2b5d32767623a7d446e53b76f3
Felecia MAKGOKA <fmakgoka@c4r9s5.wethinkcode.co.za>
2019-05-22 16:11:31 +0200

    always adding

--- libft.h
+++ libft.h
@@ -6,14 +6,16 @@
 /*   By: fmakgoka <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 14:05:02 by fmakgoka          #+#    #+#             */
-/*   Updated: 2019/05/21 15:32:19 by fmakgoka         ###   ########.fr       */
+/*   Updated: 2019/05/22 16:10:42 by fmakgoka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <stdlib.h>
 # include <unistd.h>
+# include <string.h>
 
 int		ft_tolower(int c);
 int		ft_toupper(int c);
@@ -22,5 +24,10 @@ int		ft_isdigit(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_isalnum(int c);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s1);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
+char	*ft_strcat(char *s1, const char *s2);
 
 #endif
