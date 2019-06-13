138956164bc5e6330d906041de469a707fca9b55
Felecia MAKGOKA <fmakgoka@c4r9s1.wethinkcode.co.za>
2019-06-13 14:58:26 +0200

    almost done

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,22 +6,41 @@
 /*   By: fmakgoka <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 12:22:55 by fmakgoka          #+#    #+#             */
-/*   Updated: 2019/06/12 15:19:16 by fmakgoka         ###   ########.fr       */
+/*   Updated: 2019/06/13 14:57:05 by fmakgoka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_itoa(int c)
-{
-	char *str;
-	int nb;
+#include "libft.h"
 
-	nb = 0;
-	str = (char *)malloc(sizeof(char)*());
-	if (str == NULL)
-		return (NULL);
-	if (c < 0)
+static size_t		str_len(int n)
+{
+	size_t	i;
+
+	i = 1;
+	while (n /= 10)
+		i++;
+	return (i);
+}
+
+char				*ft_itoa(int n)
+{
+	char				*str;
+	size_t				len;
+	unsigned int		cpy;
+
+	len = str_len(n);
+	cpy = n;
+	if (n < 0)
 	{
-		str[0] = '-';
-		str[1] = '\n';
+		cpy = -n;
+		len++;
 	}
+	if (!(str = ft_strnew(len)))
+		return (NULL);
+	str[--len] = cpy % 10 + '0';
+	while (cpy /= 10)
+		str[--len] = cpy % 10 + '0';
+	if (n < 0)
+		*(str + 0) = '-';
+	return (str);
 }
