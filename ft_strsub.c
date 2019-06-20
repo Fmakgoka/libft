7b2f64b709346f6024bf4835430f178217bbe340
Lucas LANGA <llanga@c5r1s8.wethinkcode.co.za>
2019-06-20 16:44:31 +0200

    strsub invalid start and len

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: fmakgoka <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 10:38:52 by fmakgoka          #+#    #+#             */
-/*   Updated: 2019/06/19 09:29:07 by fmakgoka         ###   ########.fr       */
+/*   Updated: 2019/06/20 16:43:50 by llanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 	int		i;
-	
+
 	str = (char *)malloc(sizeof(char) * (len + 1));
-	if (str == NULL || s == NULL)
+	if (str == NULL || s == NULL || start > strlen(s) || len > strlen(s))
 		return (NULL);
 	i = 0;
 	while ((size_t)i < len)
 	{
-		  str[i] = s[start];
-		  i++;
-		  start++;
+		str[i] = s[start];
+		i++;
+		start++;
 	}
 	str[i] = '\0';
 	return (str);
