29a0e48702bf68d675bcccb01b334124c70bc378
Felecia MAKGOKA <fmakgoka@c4r9s1.wethinkcode.co.za>
2019-06-21 10:47:52 +0200

    changes

--- ft_strsub.c
+++ ft_strsub.c
@@ -5,13 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmakgoka <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< HEAD
 /*   Created: 2019/06/21 10:17:21 by fmakgoka          #+#    #+#             */
-/*   Updated: 2019/06/21 10:20:49 by fmakgoka         ###   ########.fr       */
-=======
-/*   Created: 2019/06/06 10:38:52 by fmakgoka          #+#    #+#             */
-/*   Updated: 2019/06/20 16:43:50 by llanga           ###   ########.fr       */
->>>>>>> 85d9b44dc3af71967eb5f6ee88e2624f8caf463f
+/*   Updated: 2019/06/21 10:40:48 by fmakgoka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +18,7 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	int		i;
 
 	str = (char *)malloc(sizeof(char) * (len + 1));
-<<<<<<< HEAD
 	if (str == NULL || s == NULL || start > ft_strlen(s) || len > ft_strlen(s))
-=======
-	if (str == NULL || s == NULL || start > strlen(s) || len > strlen(s))
->>>>>>> 85d9b44dc3af71967eb5f6ee88e2624f8caf463f
 		return (NULL);
 	i = 0;
 	while ((size_t)i < len)
