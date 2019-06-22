7b1fb228b0d3babf57a64d563e793d9cb0f20069
Felecia MAKGOKA <fmakgoka@c5r1s10.wethinkcode.co.za>
2019-06-22 05:28:47 +0200

    ok

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,63 +6,63 @@
 /*   By: fmakgoka <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/08 10:45:34 by fmakgoka          #+#    #+#             */
-/*   Updated: 2019/06/21 15:58:25 by fmakgoka         ###   ########.fr       */
+/*   Updated: 2019/06/22 05:27:51 by fmakgoka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_counter(char const *s, char c)
+static	size_t		ft_counter(char const *s, char c)
 {
 	unsigned int	i;
 	unsigned int	ctr;
-	
+
 	i = 0;
 	ctr = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] != c && s[i] !='\0')
+		if (s[i] != c && s[i] != '\0')
 		{
 			ctr++;
 			while (s[i] != c && s[i] != '\0')
+			{
 				i++;
+			}
 		}
 		if (s[i] != '\0')
+		{
 			i++;
+		}
 	}
 	return (ctr);
 }
 
-char	**ft_strsplit(char const *s, char c)
+char				**ft_strsplit(char const *s, char c)
 {
 	char	**str;
 	size_t	i;
-	size_t	j;
+	size_t	start;
 	size_t	ctr;
-	size_t	words;
-	
+
 	i = 0;
-	j = 0;
+	start = 0;
 	ctr = 0;
-	words = ft_counter(s, c);
-	if (!(str = (char **)malloc(sizeof(char *) * words)))
+	if (!s)
 		return (NULL);
-	while (ctr < words)
+	if (!(str = (char **)malloc(sizeof(char *) * (ft_counter(s, c) + 1))))
+		return (NULL);
+	while (s[i] != '\0')
 	{
-		while (s[i] == c && s[i] != '\0')
+		if (s[i] == c)
 			i++;
-		if (s[i] != c && s[i] != '\0')
-			j = 0;
-		while (s[i + j] != c && s[i + j] != '\0')
-			j++;
-		str[ctr] = (char *)malloc(j + 1);
-		j = 0;
-		while (s[i] != c && s[i] != '\0')
+		else
 		{
-			str[ctr][j++] = s[i++];
+			start = i;
+			while (s[i] != c && s[i] != '\0')
+				i++;
+			str[ctr++] = ft_strsub(s, start, i - start);
 		}
-		str[ctr][j] = '\0';
-		ctr++;
 	}
+	str[ctr] = NULL;
 	return (str);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,36 +6,43 @@
 /*   By: fmakgoka <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 14:33:40 by fmakgoka          #+#    #+#             */
-/*   Updated: 2019/06/21 17:05:59 by fmakgoka         ###   ########.fr       */
+/*   Updated: 2019/06/22 02:06:28 by fmakgoka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s)
+static	int		white_space(char c)
+{
+	if (c == ' ' || c == '\n' || c == '\t')
+		return (1);
+	return (0);
+}
+
+char			*ft_strtrim(char const *s)
 {
 	char		*str;
 	size_t		end;
 	size_t		start;
-	size_t		size;
 	size_t		x;
 
 	if (!s)
 		return (NULL);
 	start = 0;
 	end = ft_strlen(s) - 1;
-	while (((s[start] == '\t') || (s[start] == '\n') || (s[start] == ' ') ||\
-	(s[start] == '\v'))&& s[start] != '\0')
+	if (s[0] == '\0')
+		return ((char *)s);
+	while (white_space(s[start]) && s[start] != '\0')
 		start++;
-	while (((s[end] == '\n') || (s[end] == '\t') || (s[end] == ' ') ||\
-	(s[end] == '\v')) && end >= 0)
+	if (s[start] == '\0')
+		return (ft_strcpy(ft_strnew(1), ""));
+	while (white_space(s[end]) && end >= 0)
 		end--;
-	size = end - start + 1;
-	if (!(str = (char*)malloc(sizeof(char) * (size + 1))))
+	if (!(str = (char*)malloc(sizeof(char) * ((end - start) + 2))))
 		return (NULL);
 	x = 0;
 	while (start <= end)
 		str[x++] = s[start++];
-	str[size] = '\0';
+	str[x] = '\0';
 	return (str);
 }
